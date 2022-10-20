122b0c20024bd700d1237d8b6066c72b95d5d5e0
Andres Robles <af.robles10@outlook.com>
2022-10-20 03:41:20 -0500

    portfolio v2

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -3,18 +3,18 @@ import LogoS from '../../assets/images/mepdrawing.png'
 import { useEffect, useRef } from 'react'
 import { ReactComponent as LogoSvg } from '../../assets/images/svgmep.svg'
 import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+// import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 const Logo = () => {
   const bgRef = useRef()
   const outlineLogoRef = useRef()
   const solidLogoRef = useRef()
 
   useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
+    // gsap.registerPlugin(DrawSVGPlugin)
     gsap
       .timeline()
       .to(bgRef.current, { duration: 1, opacity: 1 })
-      .from(outlineLogoRef.current, { drawSVG: 0, duration: 20 })
+      // .from(outlineLogoRef.current, { drawSVG: 0, duration: 20 })
     gsap.fromTo(
       solidLogoRef.current,
       { opacity: 0 },
